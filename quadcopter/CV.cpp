f82ee0d6bce0aaeb26690ba06b643cfd8cc0a9bb
LucasYoung <ly.youngs@gmail.com>
2018-03-05 11:27:48 -0800

    Initial commit

---
+++ quadcopter/CV.cpp
@@ -0,0 +1,12 @@
+/*
+
+*/
+
+#include <opencv2/core.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/imgproc.hpp>
+
+int calculateDistance(cv::Mat newImg, cv::Mat oldImg) {
+
+    return -1;
+}
---
+++ quadcopter/main.cpp
@@ -0,0 +1,19 @@
+/*
+
+*/
+
+#include <opencv2/core.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <chrono>
+#include <thread>
+
+int calculateDistance(cv::Mat newImg, cv::Mat oldImg);
+
+
+int main(int argc, char **argv) {
+
+
+    return 0;
+}
